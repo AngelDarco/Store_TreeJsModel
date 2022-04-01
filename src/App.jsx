45caf193526534b8dc579587fcba2ab7aefff29d
angeldarco <angel92-eme@hotmail.com>
2022-04-01 09:20:29 +0000

    Header add

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Header from './pages/header/Header'
 
 function App() {
   return (
-<>
-    <h1>App Working ..</h1>
-</>
+    <>
+      <Header/>
+    </>
   )
 }
 
---
+++ src/pages/header/Header.jsx
@@ -0,0 +1,88 @@
+import './Header.scss';
+import { AiOutlineMenu } from 'react-icons/ai';
+import { FaUserAlt } from 'react-icons/fa';
+import { BsFillTelephoneFill, BsFillCaretDownFill } from 'react-icons/bs';
+import { GrFormSearch } from 'react-icons/gr';
+import { HiShoppingCart } from 'react-icons/hi';
+
+
+const Header = () => {
+    return (
+        <>
+            <div className="containerHeader">
+                <div className="firstNav">
+                    <div className="phone">
+                        <BsFillTelephoneFill />
+                        <p className='pcall'>Call Now</p>
+                        <span className='scall'>+1 987 654 3210</span>
+                    </div>
+                    <div className="links">
+                        <p>Support</p>
+                        <p>Store Location</p>
+                        <p>Free Shopping</p>
+                    </div>
+                </div>
+
+                <div className="secondNav">
+                    <div className="title">
+                        <h2><p>e</p>commerce solution</h2>
+                    </div>
+                    <div className="input">
+                        <div className="categories">
+                            <AiOutlineMenu />
+                            <p>all categories</p>
+                            <BsFillCaretDownFill />
+                        </div>
+                        <div className="search">
+                            <input type="text" placeholder="Search new products" />
+                            <GrFormSearch/>
+                        </div>
+                    </div>
+                    <div className="user">
+                        <div className="acount">
+                            <div className="ico"><FaUserAlt/></div>
+                            <p>my account</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="tirdNav">
+                    <div className="links">
+                        <ul>
+                            <li>home</li>
+                            <li>about</li>
+                            <li>mobile</li>
+                            <li>headphone</li>
+                            <li>clothes</li>
+                            <li>shoes</li>
+                            <li>watches</li>
+                            <li>blog</li>
+                            <li>contact us</li>
+                            <li>car buy
+                                <HiShoppingCart className='ico'/>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div className="carousel">
+                        <div className="mainText">
+                            <div className="cursive">
+                                <p>- Lorem adipisicing elit. earum</p>
+                                <span></span> 
+                            </div>
+                            <h1>Lorem ipsum dolor sit amet, consectetur adipisicing</h1>
+                            <p>Lorem Quasi non tempore Quasi non tempore quia ipsum dolor sit amet, suscipit quis.</p>
+                            <button>shop now</button>
+                        </div>
+
+                    </div>
+
+                </div>
+
+
+
+            </div>
+        </>
+    )
+}
+export default Header;
