dc63d2f5e822d59544117bd07e0a87e6662d542f
angeldarco <angel92-eme@hotmail.com>
2022-10-03 06:59:19 +0000

    about add

---
+++ src/pages/about/About.jsx
@@ -0,0 +1,13 @@
+import './About.scss';
+
+const About = ()=>{
+    return(
+        <div class="aboutContainer">
+            <div class="body">
+                <h2>E-Commerce <span>Created and developed by Angel GP</span> </h2>
+                <p>Copyright © 1992-2022 Darco Corporation, All Rights Reserved.</p>
+            </div>
+        </div>
+    )
+}
+export default About;
