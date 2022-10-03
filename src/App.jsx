7f8c3fc46c48f4a7d2ca81a7498a082d116a4102
angeldarco <angel92-eme@hotmail.com>
2022-10-03 07:01:25 +0000

    App updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'
 import Header from './pages/header/Header'
 import Main from './pages/main/Main'
 import Footer from './pages/footer/Footer';
+import About from './pages/about/About';
 
 function App() {
   return (
     <>
      <Header/> 
       <Main/>
+      <About/>
       <Footer/>
     </>
   )
