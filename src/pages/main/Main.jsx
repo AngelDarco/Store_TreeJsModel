56b56bf2f4b4c6d90d5a64418709f11d21878f5e
angeldarco <angel92-eme@hotmail.com>
2024-03-23 15:29:37 -0500

    :racehorse: Main component styles updated

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -1,21 +1,19 @@
-import './Main.scss';
-import Cards from '../../conponents/cards/Cards';
+import "./Main.scss";
+import Cards from "../../components/Cards";
 
 const Main = () => {
-    return (
-        <div className="containerMain">
-            <div className="title">
-                <span></span>
-                <h2>Shop new Collection</h2>
-                <span></span>
-            </div>
-        
-            <div className="content">
-                <Cards/>
-            </div>
-        
-        </div>      
-    )
-}
-export default Main;
+  return (
+    <div className="containerMain">
+      <div className="title">
+        <span></span>
+        <h2>Shop new Collection</h2>
+        <span></span>
+      </div>
 
+      <div className="content">
+        <Cards />
+      </div>
+    </div>
+  );
+};
+export default Main;
