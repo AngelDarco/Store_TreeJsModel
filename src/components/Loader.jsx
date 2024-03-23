101708f5ba62b158a9a38d87d3eaef474d505391
angeldarco <angel92-eme@hotmail.com>
2024-03-23 16:07:52 -0500

    :sparkles: component Loader created

---
+++ src/components/Loader.jsx
@@ -0,0 +1,263 @@
+export default function Loader({ color }) {
+  return (
+    <div style={styles.container}>
+      <div style={{ ...styles.svg, fill: color }}>
+        <svg viewBox="-12 3 50 20">
+          <rect x="1" y="1" width="7.33" height="7.33">
+            <animate
+              id="spinner_oJFS"
+              begin="0;spinner_5T1J.end+0.2s"
+              attributeName="x"
+              dur="0.6s"
+              values="1;4;1"
+            />
+            <animate
+              begin="0;spinner_5T1J.end+0.2s"
+              attributeName="y"
+              dur="0.6s"
+              values="1;4;1"
+            />
+            <animate
+              begin="0;spinner_5T1J.end+0.2s"
+              attributeName="width"
+              dur="0.6s"
+              values="7.33;1.33;7.33"
+            />
+            <animate
+              begin="0;spinner_5T1J.end+0.2s"
+              attributeName="height"
+              dur="0.6s"
+              values="7.33;1.33;7.33"
+            />
+          </rect>
+          <rect x="8.33" y="1" width="7.33" height="7.33">
+            <animate
+              begin="spinner_oJFS.begin+0.1s"
+              attributeName="x"
+              dur="0.6s"
+              values="8.33;11.33;8.33"
+            />
+            <animate
+              begin="spinner_oJFS.begin+0.1s"
+              attributeName="y"
+              dur="0.6s"
+              values="1;4;1"
+            />
+            <animate
+              begin="spinner_oJFS.begin+0.1s"
+              attributeName="width"
+              dur="0.6s"
+              values="7.33;1.33;7.33"
+            />
+            <animate
+              begin="spinner_oJFS.begin+0.1s"
+              attributeName="height"
+              dur="0.6s"
+              values="7.33;1.33;7.33"
+            />
+          </rect>
+          <rect x="1" y="8.33" width="7.33" height="7.33">
+            <animate
+              begin="spinner_oJFS.begin+0.1s"
+              attributeName="x"
+              dur="0.6s"
+              values="1;4;1"
+            />
+            <animate
+              begin="spinner_oJFS.begin+0.1s"
+              attributeName="y"
+              dur="0.6s"
+              values="8.33;11.33;8.33"
+            />
+            <animate
+              begin="spinner_oJFS.begin+0.1s"
+              attributeName="width"
+              dur="0.6s"
+              values="7.33;1.33;7.33"
+            />
+            <animate
+              begin="spinner_oJFS.begin+0.1s"
+              attributeName="height"
+              dur="0.6s"
+              values="7.33;1.33;7.33"
+            />
+          </rect>
+          <rect x="15.66" y="1" width="7.33" height="7.33">
+            <animate
+              begin="spinner_oJFS.begin+0.2s"
+              attributeName="x"
+              dur="0.6s"
+              values="15.66;18.66;15.66"
+            />
+            <animate
+              begin="spinner_oJFS.begin+0.2s"
+              attributeName="y"
+              dur="0.6s"
+              values="1;4;1"
+            />
+            <animate
+              begin="spinner_oJFS.begin+0.2s"
+              attributeName="width"
+              dur="0.6s"
+              values="7.33;1.33;7.33"
+            />
+            <animate
+              begin="spinner_oJFS.begin+0.2s"
+              attributeName="height"
+              dur="0.6s"
+              values="7.33;1.33;7.33"
+            />
+          </rect>
+          <rect x="8.33" y="8.33" width="7.33" height="7.33">
+            <animate
+              begin="spinner_oJFS.begin+0.2s"
+              attributeName="x"
+              dur="0.6s"
+              values="8.33;11.33;8.33"
+            />
+            <animate
+              begin="spinner_oJFS.begin+0.2s"
+              attributeName="y"
+              dur="0.6s"
+              values="8.33;11.33;8.33"
+            />
+            <animate
+              begin="spinner_oJFS.begin+0.2s"
+              attributeName="width"
+              dur="0.6s"
+              values="7.33;1.33;7.33"
+            />
+            <animate
+              begin="spinner_oJFS.begin+0.2s"
+              attributeName="height"
+              dur="0.6s"
+              values="7.33;1.33;7.33"
+            />
+          </rect>
+          <rect x="1" y="15.66" width="7.33" height="7.33">
+            <animate
+              begin="spinner_oJFS.begin+0.2s"
+              attributeName="x"
+              dur="0.6s"
+              values="1;4;1"
+            />
+            <animate
+              begin="spinner_oJFS.begin+0.2s"
+              attributeName="y"
+              dur="0.6s"
+              values="15.66;18.66;15.66"
+            />
+            <animate
+              begin="spinner_oJFS.begin+0.2s"
+              attributeName="width"
+              dur="0.6s"
+              values="7.33;1.33;7.33"
+            />
+            <animate
+              begin="spinner_oJFS.begin+0.2s"
+              attributeName="height"
+              dur="0.6s"
+              values="7.33;1.33;7.33"
+            />
+          </rect>
+          <rect x="15.66" y="8.33" width="7.33" height="7.33">
+            <animate
+              begin="spinner_oJFS.begin+0.3s"
+              attributeName="x"
+              dur="0.6s"
+              values="15.66;18.66;15.66"
+            />
+            <animate
+              begin="spinner_oJFS.begin+0.3s"
+              attributeName="y"
+              dur="0.6s"
+              values="8.33;11.33;8.33"
+            />
+            <animate
+              begin="spinner_oJFS.begin+0.3s"
+              attributeName="width"
+              dur="0.6s"
+              values="7.33;1.33;7.33"
+            />
+            <animate
+              begin="spinner_oJFS.begin+0.3s"
+              attributeName="height"
+              dur="0.6s"
+              values="7.33;1.33;7.33"
+            />
+          </rect>
+          <rect x="8.33" y="15.66" width="7.33" height="7.33">
+            <animate
+              begin="spinner_oJFS.begin+0.3s"
+              attributeName="x"
+              dur="0.6s"
+              values="8.33;11.33;8.33"
+            />
+            <animate
+              begin="spinner_oJFS.begin+0.3s"
+              attributeName="y"
+              dur="0.6s"
+              values="15.66;18.66;15.66"
+            />
+            <animate
+              begin="spinner_oJFS.begin+0.3s"
+              attributeName="width"
+              dur="0.6s"
+              values="7.33;1.33;7.33"
+            />
+            <animate
+              begin="spinner_oJFS.begin+0.3s"
+              attributeName="height"
+              dur="0.6s"
+              values="7.33;1.33;7.33"
+            />
+          </rect>
+          <rect x="15.66" y="15.66" width="7.33" height="7.33">
+            <animate
+              id="spinner_5T1J"
+              begin="spinner_oJFS.begin+0.4s"
+              attributeName="x"
+              dur="0.6s"
+              values="15.66;18.66;15.66"
+            />
+            <animate
+              begin="spinner_oJFS.begin+0.4s"
+              attributeName="y"
+              dur="0.6s"
+              values="15.66;18.66;15.66"
+            />
+            <animate
+              begin="spinner_oJFS.begin+0.4s"
+              attributeName="width"
+              dur="0.6s"
+              values="7.33;1.33;7.33"
+            />
+            <animate
+              begin="spinner_oJFS.begin+0.4s"
+              attributeName="height"
+              dur="0.6s"
+              values="7.33;1.33;7.33"
+            />
+          </rect>
+        </svg>
+      </div>
+    </div>
+  );
+}
+
+const styles = {
+  container: {
+    width: "100%",
+    height: "100%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column",
+    padding: "40px 20px",
+  },
+  svg: {
+    fill: "white",
+    width: "50%",
+    height: "50%",
+  },
+};
