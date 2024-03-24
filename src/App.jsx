aea3fe5eda005e4fdfa60be8f153f14d9ec1f635
angeldarco <angel92-eme@hotmail.com>
2024-03-24 13:29:19 -0500

    :racehorse: new global styles added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import './App.css'
-import Header from './pages/header/Header'
-import Main from './pages/main/Main'
-import Footer from './pages/footer/Footer';
-import About from './pages/about/About';
+import "./App.css";
+import Header from "./pages/header/Header";
+import Main from "./pages/main/Main";
+import Footer from "./pages/footer/Footer";
+import About from "./pages/about/About";
 
 function App() {
   return (
-    <>
-     <Header/> 
-      <Main/>
-      <About/>
-      <Footer/>
-    </>
-  )
+    <div className="App">
+      <Header />
+      <Main />
+      <About />
+      <Footer />
+    </div>
+  );
 }
 export default App;
