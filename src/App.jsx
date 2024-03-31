a9147e966f34c97f1f2896026bcfb14818c504bc
angeldarco <angel92-eme@hotmail.com>
2024-03-31 09:23:47 -0500

    :racehorse: footer section updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,12 @@ import "./App.css";
 import Header from "./pages/header/Header";
 import Main from "./pages/main/Main";
 import Footer from "./pages/footer/Footer";
-import About from "./pages/about/About";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Main />
-      <About />
       <Footer />
     </div>
   );
