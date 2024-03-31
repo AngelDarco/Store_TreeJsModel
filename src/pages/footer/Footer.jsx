009768fb139b026aefbc3a7c689aed2105166c83
angeldarco <angel92-eme@hotmail.com>
2024-03-31 09:22:52 -0500

    :racehorse: About section merged on Footer

--- src/pages/footer/Footer.jsx
+++ src/pages/footer/Footer.jsx
@@ -1,27 +1,47 @@
-import './Footer.scss';
-import { TiSocialFacebook,TiSocialInstagram,TiSocialSkype} from 'react-icons/ti';
-import { BsGithub,BsYoutube,BsTwitter,BsWhatsapp } from 'react-icons/bs'
-import { MdOutlineAlternateEmail } from 'react-icons/md';
-import { SiGmail } from 'react-icons/si'
-import { FaTelegramPlane } from 'react-icons/fa';
+import "./Footer.scss";
+import {
+  TiSocialFacebook,
+  TiSocialInstagram,
+  TiSocialSkype,
+} from "react-icons/ti";
+import { BsGithub, BsYoutube, BsTwitter, BsWhatsapp } from "react-icons/bs";
+import { MdOutlineAlternateEmail } from "react-icons/md";
+import { SiGmail } from "react-icons/si";
+import { FaTelegramPlane } from "react-icons/fa";
 
-const Footer = ()=>{
-    return(
-        <div className="containerFooter">
-            <div className="icons">
-            <TiSocialFacebook/>
-            <TiSocialSkype/>
-            <TiSocialInstagram/>
-            <MdOutlineAlternateEmail/>
-            <SiGmail/>
-            <BsGithub/>
-            <BsYoutube/>
-            <BsTwitter/>
-            <TiSocialInstagram/>
-            <BsWhatsapp/>
-            <FaTelegramPlane/>
-            </div>
-        </div>
-    )
-}
+const Footer = () => {
+  return (
+    <div className="containerFooter">
+      <div className="icons">
+        <TiSocialFacebook />
+        <TiSocialSkype />
+        <TiSocialInstagram />
+        <MdOutlineAlternateEmail />
+        <SiGmail />
+        <BsGithub />
+        <BsYoutube />
+        <BsTwitter />
+        <TiSocialInstagram />
+        <BsWhatsapp />
+        <FaTelegramPlane />
+      </div>
+      <div className="body">
+        <h2>
+          E-Commerce{" "}
+          <span>
+            Created and developed by{" "}
+            <a
+              href="https://darco.vercel.app"
+              target="_blank"
+              style={{ color: "#f5f" }}
+            >
+              Angel GP
+            </a>
+          </span>{" "}
+        </h2>
+        <p>© 2022 Darco Corporation</p>
+      </div>
+    </div>
+  );
+};
 export default Footer;
